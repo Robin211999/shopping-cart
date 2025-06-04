5f616d9370e3a0f7eacebedc91d289947e4404e5
Amit <amit@indiawebdev.com>
2025-06-04 20:18:44 +0530

    Created a shopping cart page in react

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <a
+                        href="https://github.com/Robin211999/react-shopping-cart"
+                        className="repo_link"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        github.com/Robin211999/react-shopping-cart
+                    </a>
+
+                    <p>
+                        Built by | &nbsp;
+                        <a
+                            href="#"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Robin Goyal
+                        </a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
